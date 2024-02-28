25a54e77ec47cd0ca8a368c45a2da3668f00eae9
Dorian Baret <91059151+dobaret@users.noreply.github.com>
2024-02-28 07:59:18 +0000

    600m radius

--- docs/javascripts/map.js
+++ docs/javascripts/map.js
@@ -212,7 +212,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Draw circle around the center
         var center = L.latLng(latitude, longitude);
-        var radius = 550; // Adjust as needed
+        var radius = 600; // Adjust as needed
         var circleOptions = {
             color: 'red',
             fillColor: '#f03',
